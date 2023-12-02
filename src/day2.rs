4a6fd0077c5f48b4812d7e0cd450c6d7c7018e9f
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2023-12-02 22:21:13 +0000

    Day 2 part 2

--- src/day2.rs
+++ src/day2.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-pub fn process(input: &str) -> u32 {
+pub fn part1(input: &str) -> u32 {
     let max_red = 12;
     let max_green = 13;
     let max_blue = 14;
@@ -61,9 +61,78 @@ pub fn process(input: &str) -> u32 {
     return game_sum;
 }
 
+pub fn part2(input: &str) -> u32 {
+    let cubes_multiplication: Vec<u32> = input.lines().map(|line| {
+        let splitted_line: Vec<&str> = line.split(':').collect();
+
+        let game_id: u32 = splitted_line
+            .first()
+            .expect("Game not exists")
+            .get(5..)
+            .expect("Failed to get id")
+            .parse()
+            .expect("Failed to parse");
+
+        let mut sets: Vec<&str> = splitted_line
+            .last()
+            .expect("Game sets not exists")
+            .split(';')
+            .collect();
+
+        let mut max_red = 0;
+        let mut max_green = 0;
+        let mut max_blue = 0;
+
+        let bool_sets: Vec<bool> = sets
+            .iter_mut()
+            .map(|set| {
+                let bool_set: Vec<bool> = set
+                    .split(',')
+                    .map(|pick| {
+                        let cubes_amount: u32 = pick
+                            .get(1..3)
+                            .expect("Cubes not exists")
+                            .trim()
+                            .parse()
+                            .expect("Failed to parse");
+
+                        match pick.get(3..).expect("Cubes not exists").trim() {
+                            "red" => {
+                                if cubes_amount > max_red {
+                                    max_red = cubes_amount
+                                }
+                            }
+                            "green" => {
+                                if cubes_amount > max_green {
+                                    max_green = cubes_amount
+                                }
+                            }
+                            "blue" => {
+                                if cubes_amount > max_blue {
+                                    max_blue = cubes_amount
+                                }
+                            }
+                            _ => {}
+                        };
+
+                        return false;
+                    })
+                    .collect();
+
+                return bool_set.contains(&true);
+            })
+            .collect();
+
+        return max_blue * max_green * max_red;
+    }).collect();
+
+    return cubes_multiplication.iter().sum();
+}
+
 pub fn day2() {
     let input =
         fs::read_to_string("src/input.txt").expect("Should have been able to read the file");
 
-    println!("{}", process(&input))
+    println!("part1: {}", part1(&input));
+    println!("part2: {}", part2(&input));
 }
